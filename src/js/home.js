cb0162c40e98509ea142f128dd1c5ce2ec8825fc
Yeison Montoya <yeisondiseno@gmail.com>
2020-06-03 13:33:51 -0500

    Y.Montoya: call backs y promesas
    Signed-off-by: Yeison Montoya <yeisondiseno@gmail.com>

--- src/js/home.js
+++ src/js/home.js
@@ -1,9 +1,39 @@
 console.log('hola mundo!');
 
-const noCambia = "Leonidas";
+const getuserAll = new Promise(function(todoBien, todoMal){
+  // llamar a un api, siguientes intervals
+  //setInterval
+  setTimeout(function(){
+    // luego de 3 segundos se ejecuta 
+    todoBien('Se acabó el tiempo');
+  }, 5000)
+});
 
-let cambia = "@LeonidasEsteban"
+const getuser = new Promise(function (todoBien, todoMal) {
+  // llamar a un api, siguientes intervals
+  //setInterval
+  setTimeout(function () {
+    // luego de 3 segundos se ejecuta 
+    todoBien('Se acabó el tiempo 3');
+  }, 3000)
+});
 
-function cambiarNombre(nuevoNombre) {
-  cambia = nuevoNombre
-}
+
+// getuser
+// .then(function(){
+//   console.log('Todo está bien');
+// })
+// .catch(function(message){
+//   console.log(message);
+// })
+
+  Promise.race([
+    getuser,
+    getuser,
+  ])
+  .then(function(message){
+    console.log(message)
+  })
+  .catch(function(message){
+    console.log(message);
+  })
